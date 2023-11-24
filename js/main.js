04482bfcd44f37d99ccf3991f3d578bee065b6e9
DaviGel <geloso.davide@gmail.com>
2023-11-24 15:49:30 +0100

    feat: bonus completato

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ createApp({
   data() {
     return {
       message: 'Titolo principale della pagina',
+      imgPath: 'img/img01.jpg',
+      imgAlt: 'img01',
     };
   },
-}).mount('#title');
+}).mount('.container');
